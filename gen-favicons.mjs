d21f57a8fa8c1881b9ca938631f31cba80336ccb
DARSAN <iamspdarsan@gmail.com>
2024-09-17 04:56:11 +0530

    favicons and meta added

--- gen-favicons.mjs
+++ gen-favicons.mjs
@@ -3,78 +3,79 @@ import { mkdirSync, writeFileSync } from "fs";
 import { basename, join } from "path";
 
 export async function generateFavicons({
-  metaDesc,
-  version,
-  baseImage = "fav.png",
-  appName = basename(process.cwd()),
-  basepath = join(process.cwd(), "public"),
-  faviconsPath = "favicons",
-  devName = "Darsan",
-  devWebsite = "https://darsan.in",
+	metaDesc,
+	version,
+	baseImage = "fav.png",
+	appName = basename(process.cwd()),
+	basepath = join(process.cwd(), "public"),
+	faviconsPath = "favicons",
+	devName = "Darsan",
+	devWebsite = "https://darsan.in",
 }) {
-  const options = {
-    path: faviconsPath,
+	const options = {
+		path: faviconsPath,
 
-    appName: appName,
-    appDescription: metaDesc,
-    version: version,
+		appName: appName,
+		appDescription: metaDesc,
+		version: version,
 
-    developerName: devName,
-    developerURL: devWebsite,
+		developerName: devName,
+		developerURL: devWebsite,
 
-    background: "#fff",
-    theme_color: "#fff",
-    appleStatusBarStyle: "black-translucent",
+		background: "#fff",
+		theme_color: "#fff",
+		appleStatusBarStyle: "black-translucent",
 
-    display: "fullscreen",
-    orientation: "natural",
+		display: "fullscreen",
+		orientation: "natural",
 
-    start_url: "/",
+		start_url: "/",
 
-    manifestMaskable: true,
+		manifestMaskable: true,
 
-    icons: {
-      android: true,
-      appleIcon: true,
-      favicons: true,
-      yandex: true,
-      appleStartup: false,
-      windows: false,
-    },
+		icons: {
+			android: true,
+			appleIcon: true,
+			favicons: true,
+			yandex: true,
+			appleStartup: false,
+			windows: false,
+		},
 
-    shortcuts: {},
-  };
+		shortcuts: {},
+	};
 
-  try {
-    const { images, files, html } = await favicons(baseImage, options);
+	try {
+		const { images, files, html } = await favicons(baseImage, options);
 
-    const rootPath = join(basepath, faviconsPath);
-    mkdirSync(rootPath, { recursive: true });
+		const rootPath = join(basepath, faviconsPath);
+		mkdirSync(rootPath, { recursive: true });
 
-    /* write images */
-    images.forEach((image) => {
-      writeFileSync(join(rootPath, image.name), image.contents, {
-        encoding: "binary",
-      });
-    });
+		/* write images */
+		images.forEach((image) => {
+			writeFileSync(join(rootPath, image.name), image.contents, {
+				encoding: "binary",
+			});
+		});
 
-    /* write config files */
-    files.forEach((file) => {
-      writeFileSync(join(rootPath, file.name), file.contents, {
-        encoding: "utf8",
-      });
-    });
+		/* write config files */
+		files.forEach((file) => {
+			writeFileSync(join(rootPath, file.name), file.contents, {
+				encoding: "utf8",
+			});
+		});
 
-    const faviconsHtmlLinks = html.join("");
-    writeFileSync(join(basepath, "links.html"), faviconsHtmlLinks);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+		const faviconsHtmlLinks = html.join("");
+		writeFileSync(join(basepath, "links.html"), faviconsHtmlLinks);
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
 }
 
 generateFavicons({
-  metaDesc: "pending",
-  version: "0.1.0",
-  appName: "Effective PC",
+	metaDesc:
+		"Discover custom-built PCs tailored for gamers and startups at Effective PC. We deliver high-performance, budget-friendly systems that maximize efficiency and power up your gaming or business operations. Get your custom PC today at effectivepc.in.",
+	version: "0.1.0",
+	appName: "Effective PC",
 });
