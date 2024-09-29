4205cf05ea9a0a4447bad94847dd615e3cef9589
DARSAN <iamspdarsan@gmail.com>
2024-09-30 01:56:19 +0530

    cube cpu added

--- components/cube-slide.tsx
+++ components/cube-slide.tsx
@@ -3,33 +3,35 @@ import "@/styles/cube.scss";
 export default function CubeSlide() {
 	return (
 		<div id="cube-slide">
-			<div className="swiper">
+			<div
+				className="swiper"
+				dir="rtl">
 				<div className="swiper-wrapper">
 					<div className="swiper-slide">
 						<img
-							src="./side-1.jpg"
-							alt=""
+							src="/src/front.png"
+							alt="Effective PC - CPU/PC Model Front Side"
 						/>
 					</div>
 
 					<div className="swiper-slide">
 						<img
-							src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/2d165721-fe2e-4cf0-a63e-20bc5bc3f847"
-							alt=""
+							src="/src/side-left.png"
+							alt="Effective PC - CPU/PC Model Left Side"
 						/>
 					</div>
 
 					<div className="swiper-slide">
 						<img
-							src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/d311d1de-7382-4c03-b083-5f7e88458158"
-							alt=""
+							src="/src/back.png"
+							alt="Effective PC - CPU/PC Model Back Side"
 						/>
 					</div>
 
 					<div className="swiper-slide">
 						<img
-							src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/be223a30-52d1-4a0b-8d57-2e52f02e2245"
-							alt=""
+							src="/src/side-right.png"
+							alt="Effective PC - CPU/PC Model Right Side"
 						/>
 					</div>
 				</div>
